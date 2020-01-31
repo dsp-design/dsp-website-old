2495a4dcf7161f2abdc2735c415f57db22b8bd33
hassan-baig <hassanbaigdev@gmail.com>
2020-01-31 06:07:09 +0500

    6th change

--- dsp-splash-page/src/pages/People/Trifold/TriFold.js
+++ dsp-splash-page/src/pages/People/Trifold/TriFold.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import trifold1 from "./assets/images/trifold_1.png";
+import trifold2 from "./assets/images/trifold_2.png";
+import trifold3 from "./assets/images/trifold_3.png";
+import "../../Clients/assets/css/clientPagesCSS/clientPages.css";
+import "../../Clients/assets/css/clientPagesCSS/mediaClientPages.css";
+
 
 class TriFold extends Component {
   state = {  }
@@ -7,19 +13,19 @@ class TriFold extends Component {
     return ( 
       <React.Fragment>
         <div className='row mx-0'>
-          <div className='col-lg-4 col-md-4 col-sm-12 my-2'>
+          <div className='col-4 my-2 text-right'>
             <Link to="../clients/designbuild/trifold">
-              <img src={require('./assets/images/trifold_1.png')} width="100%"/>
+              <img src={trifold1} alt="trifold" className="miniTrifold" style={{position:"initial"}}/>
             </Link>
           </div>
-          <div className='col-lg-4 col-md-4 col-sm-12 my-2'>
+          <div className='col-4 my-2 text-center'>
             <Link to="../clients/largecompany/trifold">
-              <img src={require('./assets/images/trifold_2.png')} width="100%"/>
+              <img src={trifold2} alt="trifold" className="miniTrifold" style={{ position:"initial"}}/>
             </Link>
           </div>
-          <div className='col-lg-4 col-md-4 col-sm-12 my-2'>
+          <div className='col-4 my-2 text-left'>
             <Link to="../clients/boutiquepractice/trifold">
-              <img src={require('./assets/images/trifold_3.png')} width="100%"/>
+              <img src={trifold3} alt="trifold" className="miniTrifold" style={{ position:"initial"}}/>
             </Link>
           </div>
         </div>
